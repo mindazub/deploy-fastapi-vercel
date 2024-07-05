6df532432cad555989f69e08d81261eae8eeb77a
Mindaugas <mindaugas.azubalis@gmail.com>
2024-07-05 15:05:38 +0300

    Penkto pakanka įsitikinti!

--- main.py
+++ main.py
@@ -5,19 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Hello World"}
-    
-@app.get("/third")
-def home():
-    return {"message": "Hello third"}
+    return {"message": "Taigi penkto uzteks. Pakanka tik git push -u origin main."}
 
-@app.get("/frogs")
-def home():
-    return {"message": "Hello frogs"}
-
-@app.get("/fourth")
-def home():
-    return {"message": "May the fourth be with you!"}
 
 
     
